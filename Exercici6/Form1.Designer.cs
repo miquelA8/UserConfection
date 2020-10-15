d068f27868a275a953b004932ef950fbd344ee73
miquelA8 <miquelangellopez@paucasesnovescifp.cat>
2020-10-15 23:24:17 +0200

    Exercice 7 up
    In this exercice we have a box with diferents colours and a text box.
    The text box shows the colour selected.

---
+++ Exercici6/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Exercici6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.colorBox = new System.Windows.Forms.ComboBox();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // colorBox
+            // 
+            this.colorBox.FormattingEnabled = true;
+            this.colorBox.Items.AddRange(new object[] {
+            "Red",
+            "Green",
+            "Blue",
+            "Cian",
+            "Magent"});
+            this.colorBox.Location = new System.Drawing.Point(294, 111);
+            this.colorBox.Name = "colorBox";
+            this.colorBox.Size = new System.Drawing.Size(121, 21);
+            this.colorBox.TabIndex = 0;
+            this.colorBox.SelectedIndexChanged += new System.EventHandler(this.colorBox_SelectedIndexChanged);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(179, 292);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(426, 20);
+            this.textBox.TabIndex = 1;
+            this.textBox.Text = "The choosen colour is:  ";
+            this.textBox.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.colorBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox colorBox;
+        private System.Windows.Forms.TextBox textBox;
+    }
+}
+
---
+++ Exercici6/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercici6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void colorBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String fill = "The choosen colour is: ";
+
+            textBox.Text = fill + colorBox.SelectedItem;
+        }
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
